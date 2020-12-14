4ca1ccc795946ffb1f6f2da0ee1aeab17cdfb476
ithater <ithater.iwnl@gmail.com>
2020-12-14 10:42:52 +0300

    cr: motto

--- src/index.js
+++ src/index.js
@@ -8,3 +8,24 @@ import header from './blocks/header/header';
 
 
 header();
+
+
+const arr = [[10]];
+
+
+const number = arr[0];
+console.log('number: ', number);
+
+
+arr[0].push('a string');
+console.log('number: ', number);
+console.log('arr: ', arr);
+
+
+
+
+
+
+
+
+
