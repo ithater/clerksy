acb7266dd275c02baae2c0e89ca03b9bbd753710
ithater <ithater.iwnl@gmail.com>
2020-12-14 14:31:31 +0300

    cr: promo

--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
 import 'normalize.css';
 import './index.sass';
 
-
 import 'loading-attribute-polyfill';
 
 import header from './blocks/header/header';
 
 
 header();
-
-
-const arr = [[10]];
-
-
-const number = arr[0];
-console.log('number: ', number);
-
-
-arr[0].push('a string');
-console.log('number: ', number);
-console.log('arr: ', arr);
-
-
-
-
-
 
 
 
